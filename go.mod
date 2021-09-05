f9d44d859c66fb87d4e0bc7894d2cafa6284d632
urnikrokhiyah <urnikrokhiyah123@gmail.com>
2021-09-05 17:43:55 +0700

    Add user and book models

---
+++ go.mod
@@ -0,0 +1,9 @@
+module cleancode
+
+go 1.16
+
+require (
+	github.com/labstack/echo/v4 v4.5.0 // indirect
+	gorm.io/driver/mysql v1.1.2 // indirect
+	gorm.io/gorm v1.21.14 // indirect
+)
---
+++ models/user.go
@@ -0,0 +1,16 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+	Token    string `json:"token" form:"token"`
+}
+
+type OutputUser struct {
+	Username string
+	Email    string
+}
